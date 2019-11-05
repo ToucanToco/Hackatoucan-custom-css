73b3ccde8db2a93f7b499e27557b928e199d5d41
tmartin42 <japremieres2@gmail.com>
2019-11-05 13:53:31 +0100

    add smallappIds

--- server.js
+++ server.js
@@ -36,6 +36,21 @@ router.register('/parseDiff', function(req, res) {
   res.end();
 });
 
+router.register('/affectedSmallApps', function(req, res) {
+  let diffText = require('./diff.txt');
+
+  let smallApps = require('./smallApps.json');
+
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write(
+    `<body>${smallApps.reduce((acc, smallApp) => {
+      return acc + `<a href="/smallAppCustomCss?name=${smallApp.id}">${smallApp.id}</a><br/>`;
+    }, '')}</body>`
+  );
+  res.end();
+});
+
+
 // We need a server which relies on our router
 var server = http.createServer(function (req, res) {
   handler = router.route(req);

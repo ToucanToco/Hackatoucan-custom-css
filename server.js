6db02ebbb7cfe03f28ac378c105edf7c2b175418
tmartin42 <japremieres2@gmail.com>
2019-11-05 12:13:03 +0100

    add parser

--- server.js
+++ server.js
@@ -8,6 +8,18 @@ require.extensions['.txt'] = function (module, filename) {
   module.exports = fs.readFileSync(filename, 'utf8');
 };
 
+function cssPropertiesParser(cssTxt, isDiff)  {
+  let regex = isDiff ? /(?:\+?.*(\.\S+)\s+{)\n(?:.*;\n)*(?:\+ .*;)/gm : /.*(\.\S+)\s+{/gm;
+
+  let properties = [];
+
+  while ((match = regex.exec(cssTxt)) !== null) {
+    properties.push(match[1]);
+  }
+  
+  return properties.toString();
+}
+
 // Handle your routes here, put static pages in ./public and they will server
 router.register('/', function(req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
@@ -16,11 +28,11 @@ router.register('/', function(req, res) {
 });
 
 // Handle your routes here, put static pages in ./public and they will server
-router.register('/payload', function(req, res) {
+router.register('/parseDiff', function(req, res) {
   let diffText = require('./diff.txt');
 
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.write(diffText);
+  res.write(cssPropertiesParser(diffText, true));
   res.end();
 });
 

239f9183117035d68b9518f9084ae31c8b2d1f67
tmartin42 <japremieres2@gmail.com>
2019-11-05 14:33:20 +0100

    add get customcss route

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
-let sys = require('sys');
-let http = require('http');
-let router = require('./router');
-let fs = require('fs');
-
+const sys = require('sys');
+const http = require('http');
+const router = require('./router');
+const fs = require('fs');
+const url = require('url');
+const request = require('request');
 
 require.extensions['.txt'] = function (module, filename) {
   module.exports = fs.readFileSync(filename, 'utf8');
@@ -50,6 +51,30 @@ router.register('/affectedSmallApps', function(req, res) {
   res.end();
 });
 
+router.register('/smallAppCustomCss', function(req, res) {
+  const res2 = res;
+  if (req.method === 'GET') {
+    const currentUrl = new url.parse(req.url);
+    const params = /.+name=(.+$)/.exec(currentUrl.search);
+
+    if (params && params[1]) {
+      const smallAppId = params[1];
+      request(`https://api-demo-staging.toucantoco.com/${smallAppId}/assets/styles/variables`, {}, (err, res, body) =>
+        {
+          if (err) {return console.log(err);}
+          res2.writeHead(200, {'Content-Type': 'application/json'});
+          res2.write(body);
+          res2.end();
+        }
+      );
+
+    } else {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(`invalid param: ${currentUrl.search}`);
+      res.end();
+    }
+  }
+});
 
 // We need a server which relies on our router
 var server = http.createServer(function (req, res) {
